5bb79c33a137a36ba8d07724da97248935a41e9d
byunsangun <xdcfvgg@naver.com>
2020-11-07 02:01:34 +0900

    메인화면, 영화 상세 화면 60%

---
+++ Watcha/movieDetail/movieSimilarTable/MovieSimilarCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieSimilarCollectionViewCell.swift
+//  Watcha
+//
+//  Created by 변상운 on 2020/11/06.
+//  Copyright © 2020 sangun. All rights reserved.
+//
+
+import UIKit
+
+class MovieSimilarCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Watcha/movieDetail/movieSimilarTable/MovieSimilarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSimilarTableViewCell.swift
+//  Watcha
+//
+//  Created by 변상운 on 2020/11/06.
+//  Copyright © 2020 sangun. All rights reserved.
+//
+
+import UIKit
+
+class MovieSimilarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
