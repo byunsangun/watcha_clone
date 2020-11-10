e5886c4b1263f1e6f95c3768389fd2e0482a8504
byunsangun <xdcfvgg@naver.com>
2020-11-11 02:20:12 +0900

    홈화면 장르별 API 호출 마무리, 상세화면 디테일 보완

---
+++ Watcha/movieDetail/DramaEpisodeTable/DramaEpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DramaEpisodeTableViewCell.swift
+//  Watcha
+//
+//  Created by 변상운 on 2020/11/10.
+//  Copyright © 2020 sangun. All rights reserved.
+//
+
+import UIKit
+
+class DramaEpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
